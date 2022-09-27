8b383302988ac1393c7fbf4761ae2147c917da8a
Sulaiman Tahir Idris <sulaymanti01@gmail.com>
2022-09-27 18:19:24 +0100

    vuala

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_memset -   fills memory with a constant byte
+ *_memset - fills memory with a constant byte
  *@s: pointer block of memory to fill
  *@b: value to set
  *@n: bytes of the memory
@@ -10,14 +10,12 @@
 char *_memset(char *s, char b, unsigned int n)
 
 {
-	unsigned int i;
+  unsigned int i;
 
-	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b; /*add 1 position s*/
+  for (i = 0; i <n; i++)
+    {
+      *(s + i) = b;
+    }
 
-	} /*END FOR*/
-
-	return (s);
-}
+  return (s);
+  
